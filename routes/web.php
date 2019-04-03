3599fdef28755e9c208c8b9c96b21456a25ce432
Thales Serra <thales.serra97@gmail.com>
2019-04-03 17:50:54 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'admin'], function () {
+  Route::get('/login', 'AdminAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'AdminAuth\LoginController@login');
+  Route::post('/logout', 'AdminAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'AdminAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'AdminAuth\RegisterController@register');
+
+  Route::post('/password/email', 'AdminAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'AdminAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'AdminAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'AdminAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'candidate'], function () {
+  Route::get('/login', 'CandidateAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'CandidateAuth\LoginController@login');
+  Route::post('/logout', 'CandidateAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'CandidateAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'CandidateAuth\RegisterController@register');
+
+  Route::post('/password/email', 'CandidateAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'CandidateAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'CandidateAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'CandidateAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'company'], function () {
+  Route::get('/login', 'CompanyAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'CompanyAuth\LoginController@login');
+  Route::post('/logout', 'CompanyAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'CompanyAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'CompanyAuth\RegisterController@register');
+
+  Route::post('/password/email', 'CompanyAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'CompanyAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'CompanyAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'CompanyAuth\ResetPasswordController@showResetForm');
+});
